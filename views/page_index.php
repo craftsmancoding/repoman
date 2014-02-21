e25e5004de4c70a56b4592e7522627eda5b74935
dan.edan <INTEL@INTEL-PC.(none)>
2014-02-21 10:25:35 +0800

    Update home format

--- views/page_index.php
+++ views/page_index.php
@@ -1,3 +1,15 @@
+<style>
+	.repo-wrapper a,
+	.repo-wrapper h2 {
+		float: left;
+	}
+	.repo-wrapper h2 {
+		margin-right: 20px;
+	}
+	.repo-wrapper a {
+		margin-top: 25px;
+	}
+</style>
 <form action="<?php print $this->getUrl('home'); ?>" method="post">
 
 	<h4 style="margin-top:20px;">Repository Path</h4>
@@ -5,13 +17,15 @@
     <div class="repoman_settings <?php print $class;?>">
         <?php print $msg; ?>
     	<code>MODX_BASE_PATH/</code><input type="text" name="repoman_dir" value="<?php print $this->modx->getOption('repoman.dir'); ?>" size="60"/>
+    	<input type="submit" class="repoman_button" value="Update Setting" />	
     </div>
-	<input type="submit" class="repoman_button" value="Update Setting" />		
+		
 
 </form>
+<div class="repo-wrapper">
+	<h2 style="margin-top:20px;">Your Repositories</h2>
+	<a href="<?php print $this->getUrl('create'); ?>" class="repoman_button">Create New Repo</a>
+</div>
 
-<a href="<?php print $this->getUrl('create'); ?>" class="repoman_button">Create New Repo</a>
-
-<h2 style="margin-top:20px;">Your Repositories</h2>
 
 <?php print $repos; ?>

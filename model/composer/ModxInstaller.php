c2bcbd097b75fa8d80f95ae2dc0e98ec8530f3d0
Everett Griffiths <everett@fireproofsocks.com>
2014-08-18 15:06:22 -0700

    Testing...

--- model/composer/ModxInstaller.php
+++ model/composer/ModxInstaller.php
@@ -20,7 +20,7 @@ class ModxInstaller extends LibraryInstaller
      */
     public function getPackageBasePath(PackageInterface $package)
     {
-
+        error_log(__FUNCTION__);
 /*
         $prefix = substr($package->getPrettyName(), 0, 23);
         if ('phpdocumentor/template-' !== $prefix) {
@@ -40,6 +40,74 @@ class ModxInstaller extends LibraryInstaller
      */
     public function supports($packageType)
     {
+        error_log(__FUNCTION__);
         return 'modx-package' === $packageType;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        error_log(__CLASS__.'::'.__FUNCTION__);
+        error_log('Repo: '.print_r($repo,true));
+        error_log('Package: '.print_r($package,true));
+        return parent::install($repo,$package);
+/*
+        $extra = $package->getExtra();
+        if (empty($extra['class'])) {
+            print var_dump($extra);
+            throw new \UnexpectedValueException('Error while installing '.$package->getPrettyName().', composer-plugin packages should have a class defined in their extra key to be usable.');
+        }
+
+        parent::install($repo, $package);
+        $this->composer->getPluginManager()->registerPackage($package);
+*/
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+    {
+        error_log(print_r($repo,true));
+        error_log(print_r($initial,true));
+        error_log(print_r($target,true));
+        return parent::update($repo,$initial,$target);    
+/*
+        $extra = $target->getExtra();
+        if (empty($extra['class'])) {
+            throw new \UnexpectedValueException('Error while installing '.$target->getPrettyName().', composer-plugin packages should have a class defined in their extra key to be usable.');
+        }
+
+        parent::update($repo, $initial, $target);
+        $this->composer->getPluginManager()->registerPackage($target);
+*/
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        return parent::uninstall($repo, $package);
+/*
+        if (!$repo->hasPackage($package)) {
+            throw new \InvalidArgumentException('Package is not installed: '.$package);
+        }
+
+        $this->removeCode($package);
+        $this->removeBinaries($package);
+        $repo->removePackage($package);
+
+        $downloadPath = $this->getPackageBasePath($package);
+        if (strpos($package->getName(), '/')) {
+            $packageVendorDir = dirname($downloadPath);
+            if (is_dir($packageVendorDir) && $this->filesystem->isDirEmpty($packageVendorDir)) {
+                @rmdir($packageVendorDir);
+            }
+        }
+*/
+    }
+    
 }
--- model/composer/ModxInstallerPlugin.php
+++ model/composer/ModxInstallerPlugin.php
@@ -5,12 +5,11 @@ namespace Repoman\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-error_log(__FILE__ .' included.');
+
 class ModxInstallerPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
-        error_log('Testing... '.__CLASS__);
         $installer = new ModxInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }

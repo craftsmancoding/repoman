668c20acf56beba29f3cf9d4d7f30b5b30d8b23e
Everett Griffiths <everett@fireproofsocks.com>
2014-08-18 13:55:15 -0700

    Working on composer.

--- model/composer/ModxInstallerPlugin.php
+++ model/composer/ModxInstallerPlugin.php
@@ -5,7 +5,7 @@ namespace Repoman\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-
+error_log(__FILE__ .' included.');
 class ModxInstallerPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
